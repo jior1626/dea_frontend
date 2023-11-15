6187b45320b008fce4115ff6ab792846813c9ac0
inteligenciafutura <inteligenciafuturatv@gmail.com>
2023-11-15 14:49:16 -0500

    agenda dinamica

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -9,28 +9,46 @@ import React, { useEffect, useState } from 'react';
 import { Modal } from '../components/Modal';
 
 export default function Options() {
-    const router = useRouter();
+  const router = useRouter();
 
-    const navigateToSectionAgendar = () => {
-        router.push("agendar");
-      };
+  const navigateToSectionAgendar = () => {
+    router.push("agendar");
+  };
 
-      const navigateToSectionActavisita = () => {
-        router.push("actavisita");
-      };
+  const navigateToSectionActavisita = () => {
+    router.push("actavisita");
+  };
+
+  // Determina el ancho del cajón basado en el tamaño de la ventana
+  const [windowWidth, setWindowWidth] = useState(0);
+  useEffect(() => {
+    const updateWindowWidth = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', updateWindowWidth);
+    updateWindowWidth();
+
+    return () => window.removeEventListener('resize', updateWindowWidth);
+  }, []);
+
+  const cajonStyle = {
+    width: windowWidth >= 768 ? '768px' : '100%', // Ancho fijo para escritorio
+    margin: '0 auto', // Centrado horizontal
+  };
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between">
+    <main className="flex min-h-screen flex-col items-center justify-between" >
       <Header></Header>
 
       <div className="min-h-[700px] flex items-center justify-center mt-8">
-        <div className="bg-white rounded-lg p-8 shadow-md max-w-md mx-5 w-full mb-8">
+        <div className="bg-white rounded-lg shadow-md p-8 mx-5 mb-8" style={cajonStyle}>
 
-        <h2 className="text-center text-black">
-        <strong>AGENDA</strong>
-        </h2>
+          <h2 className="text-center text-black">
+            <strong>AGENDA</strong>
+          </h2>
 
-        <button
+          <button
             className="mt-4 w-full bg-custom-purple text-white py-3 rounded-full shadow-lg"
             onClick={navigateToSectionAgendar}
           >
@@ -46,7 +64,7 @@ export default function Options() {
 
         </div>
       </div>
-   
+
 
       <Footer></Footer>
     </main>
--- app/agendar/page.tsx
+++ app/agendar/page.tsx
@@ -9,90 +9,73 @@ import React, { useEffect, useState } from 'react';
 import { Modal } from '../components/Modal';
 
 export default function Options() {
-    const router = useRouter();
+  const router = useRouter();
 
-      const navigateToSectionActavisita = () => {
-        router.push("actavisita");
-      };
+  const [fields, setFields] = useState([]);
+  const [formData, setFormData] = useState({});
 
-  const [AGE_TITULO, setAGE_TITULO] = useState("");
-  const [AGE_DESCRIPCION, setAGE_DESCRIPCION] = useState("");
-  const [AGE_FECHA_INICIO, setAGE_FECHA_INICIO] = useState("");
-  const [AGE_FECHA_FIN, setAGE_FECHA_FIN] = useState("");
-  const [AGE_UBICACION, setAGE_UBICACION] = useState("");
-  const [AGE_TODODIA, setAGE_TODODIA] = useState(false);
-  const [AGE_RECURRENTE, setAGE_RECURRENTE] = useState(false);
-  const [AGE_FRECUENCIA, setAGE_FRECUENCIA] = useState("Semanal");
-  const [AGE_PRIORIDAD, setAGE_PRIORIDAD] = useState("Media");
-  const [AGE_PARTICIPANTES, setAGE_PARTICIPANTES] = useState("");
+  const handleInputChange = (e) => {
+    setFormData({
+        ...formData,
+        [e.target.name]: e.target.value
+    });
+};
+
+  useEffect(() => {
+    // Obtener los campos de la API al cargar el componente
+    fetch('https://dea-crud.azurewebsites.net/apiAgenda.php?fields')
+      .then(response => response.json())
+      .then(data => {
+        // Filtrar para excluir el campo 'id'
+        const filteredFields = data.filter(field => field !== 'id');
+        setFields(filteredFields);
+      })
+      .catch(error => {
+        // Manejo de errores aquí
+        console.error('Error al obtener los campos:', error);
+      });
+  }, []);
 
 
   const handleSubmit = async () => {
     const jwt = localStorage.getItem("jwt");
-
-    // Validar que todos los campos estén llenos
-    if (
-      !AGE_TITULO ||
-      !AGE_DESCRIPCION ||
-      !AGE_FECHA_INICIO ||
-      !AGE_FECHA_FIN ||
-      !AGE_UBICACION ||
-      !AGE_TODODIA ||
-      !AGE_RECURRENTE ||
-      !AGE_FRECUENCIA ||
-      !AGE_PRIORIDAD ||
-      !AGE_PARTICIPANTES
-    ) {
-      alert("Por favor, complete todos los campos.");
-      return;
-    }
-
-    // Construir el objeto de datos a enviar
-    const data = {
-      data: {
-        AGE_TITULO:AGE_TITULO,
-        AGE_DESCRIPCION:AGE_DESCRIPCION,
-        AGE_FECHA_INICIO:AGE_FECHA_INICIO,
-        AGE_FECHA_FIN:AGE_FECHA_FIN,
-        AGE_UBICACION:AGE_UBICACION,
-        AGE_TODODIA:AGE_TODODIA,
-        AGE_RECURRENTE:AGE_RECURRENTE,
-        AGE_FRECUENCIA:AGE_FRECUENCIA,
-        AGE_PRIORIDAD:AGE_PRIORIDAD,
-        AGE_PARTICIPANTES:AGE_PARTICIPANTES
-    }
-    };
-
-    //alert(JSON.stringify(data))
-
-    // Realizar la solicitud POST
-    try {
-      const jsonBody = JSON.stringify(data);
-      const response = await fetch(
-        "https://nodosalud1.com/api/tbl-agendas",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${jwt}`, // Agregar el token JWT al encabezado
-          },
-          body: JSON.stringify(data),
+    // Validar que todos los campos están llenos
+    for (const field of fields) {
+        if (!formData[field]) {
+            alert(`Por favor, completa el campo ${field}`);
+            return;
         }
-      );
-      //alert(JSON.stringify(data))
-      if (response.ok) {
-        // La solicitud se completó con éxito
-        // Puedes redirigir a otra página o realizar otras acciones aquí
-        router.push("registroexitosoanexo2"); // Cambia '/otra-pagina' a la URL a la que deseas redirigir
-        alert("Agenda registrada");
-      } else {
-        // Hubo un error en la solicitud
-        alert("Error al enviar los datos.");
-      }
-    } catch (error) {
-      console.error("Error en la solicitud:", error);
     }
-  };
+
+    try {
+        const response = await fetch('https://dea-crud.azurewebsites.net/apiAgenda.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                // Agrega aquí otros headers necesarios, como autorización
+                'Authorization': `Bearer ${jwt}` // Ejemplo de cómo incluir el JWT, ajusta según sea necesario
+            },
+            body: JSON.stringify(formData)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const result = await response.json();
+
+        // Verifica si el mensaje de la respuesta es 'Registro creado'
+        if (result.message === 'Registro creado') {
+            // Redirige a la página 'agenda'
+            router.push("agenda");
+        } else {
+            console.log(result); // O maneja los mensajes de error como sea necesario
+        }
+    } catch (error) {
+        console.error('Error al enviar el formulario:', error);
+    }
+};
+
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between">
@@ -101,22 +84,33 @@ export default function Options() {
       <div className="min-h-[700px] flex items-center justify-center mt-8">
         <div className="bg-white rounded-lg p-8 shadow-md max-w-md mx-5 w-full mb-8">
 
-        <h2 className="text-center text-black">
-        <strong>AGENDAR</strong>
-        </h2>
+          <h2 className="text-center text-black">
+            <strong>AGENDAR</strong>
+          </h2>
 
-       
+          {/* Generar inputs de forma dinámica */}
+          {fields.map(field => (
+            <input
+            key={field}
+            type="text"
+            name={field}
+            placeholder={field}
+            className="mt-4 w-full py-2 px-4 rounded-full border-2"
+            onChange={handleInputChange}
+        />
+          ))}
 
-        <button
+
+          <button
             className="mt-4 w-full bg-custom-purple text-white py-3 rounded-full shadow-lg"
             onClick={handleSubmit}
-        >
-          Agendar
-        </button>
+          >
+            Agendar
+          </button>
 
         </div>
       </div>
-   
+
 
       <Footer></Footer>
     </main>
