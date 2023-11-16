2fc0b0dd20ab22e0a2e51e18accefd53ab0f0e90
tsbedoya <ts231706@gmail.com>
2023-11-16 12:33:44 -0500

    anexofinalizo

--- app/anexo3finalizo/page.tsx
+++ app/anexo3finalizo/page.tsx
@@ -10,7 +10,7 @@ export default function Anexo3Finalizo() {
   const router = useRouter();
 
   
-  const handleLogin = async () => {
+  const handleAnexo8 = async () => {
     // La solicitud fue exitosa, puedes redirigir a la página deseada
     router.push("options");
   };
@@ -19,8 +19,8 @@ export default function Anexo3Finalizo() {
     <main className="flex flex-col items-center justify-between">
       <Header></Header>
 
-      <div className="mt-32 min-h-[400px] flex items-center justify-center">
-        <div className="bg-white rounded-lg p-8 shadow-md max-w-md mx-5 w-full text-center">
+      <div className="min-h-[400px] flex items-center justify-center container">
+        <div className="bg-white rounded-lg p-8 shadow-md max-w-md mx-5 w-full text-center ">
           <div className="mx-auto">
             <img
               src="https://nivel99.com/desfibriladores/circle-check-regular 1.png"
@@ -29,7 +29,7 @@ export default function Anexo3Finalizo() {
           </div>
           <br></br>
           <br></br>
-          <img src="https://nivel99.com/desfibriladores/botonmorado.png" style={{ margin: "0 auto" }} onClick={handleLogin} />
+          <img src="https://nivel99.com/desfibriladores/botonmorado.png" style={{ margin: "0 auto" }} onClick={handleAnexo8} />
           <br></br>
           <br></br>
           <p className="text-black">Se notificará vía correo electrónico el estado de su aprobación.</p>
