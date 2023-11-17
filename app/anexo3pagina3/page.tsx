b38a7b87891905e769a60a87057fc9e248a26377
inteligenciafutura <38333399+inteligenciafutura@users.noreply.github.com>
2023-11-17 15:00:04 -0500

    post de uso de dea

--- app/anexo3pagina3/page.tsx
+++ app/anexo3pagina3/page.tsx
@@ -45,96 +45,30 @@ export default function Options() {
     minute: "2-digit",
   });
 
-  const [
-    uso_datoseventonombrepersonautilizodea,
-    setUso_datoseventonombrepersonautilizodea,
-  ] = useState("");
-  const [uso_nombrelugarevento, setUso_nombrelugarevento] = useState("");
-  const [
-    uso_personaatendidaeventotipodocumento,
-    setUso_personaatendidaeventotipodocumento,
-  ] = useState("");
-  const [
-    uso_personaatendidaeventonumerodocumento,
-    setUso_personaatendidaeventonumerodocumento,
-  ] = useState("");
-  const [uso_personaatendidaeventoedad, setUso_personaatendidaeventoedad] =
-    useState("");
-  const [uso_personaatendidaeventosexo, setUso_personaatendidaeventosexo] =
-    useState("");
-  const [
-    uso_personaatendidaeventoaseguradorsalud,
-    setUso_personaatendidaeventoaseguradorsalud,
-  ] = useState("");
-  const [uso_datoseventotipodocumento, setUso_datoseventotipodocumento] =
-    useState("");
-  const [uso_datoseventonumerodocumento, setUso_datoseventonumerodocumento] =
-    useState("");
-
-  const [uso_datoseventohorainicioevento, setUso_datoseventohorainicioevento] =
-    useState("" + horaActual);
-  const [
-    uso_datoseventohoraactivacioncadenasupervivencia,
-    setUso_datoseventohoraactivacioncadenasupervivencia,
-  ] = useState("" + horaActual);
-  const [
-    uso_datoseventohorautilizaciondea,
-    setUso_datoseventohorautilizaciondea,
-  ] = useState("" + horaActual);
-  const [
-    uso_datoseventohoratrasladopersonaatendida,
-    setUso_datoseventohoratrasladopersonaatendida,
-  ] = useState("" + horaActual);
-  const [
-    uso_datosmediotransportenombreencargado,
-    setUso_datosmediotransportenombreencargado,
-  ] = useState("");
-  const [
-    uso_datosmediotransportemediotransporte,
-    setUso_datosmediotransportemediotransporte,
-  ] = useState("");
-  const [
-    uso_datosmediotransporteempresaambulancia,
-    setUso_datosmediotransporteempresaambulancia,
-  ] = useState("");
-  const [
-    uso_datosmediotransporteobservaciones,
-    setUso_datosmediotransporteobservaciones,
-  ] = useState("");
-  const [uso_FUso_firma, setUso_firma] = useState("");
-
-  // Estados para cada uno de los campos del formulario y para los errores
-  const [nombreCompleto, setNombreCompleto] = useState("");
-  const [documentoIdentificacion, setDocumentoIdentificacion] = useState("");
-  const [existenciaDesfibriladores, setExistenciaDesfibriladores] =
-    useState("1");
-  const [nombreSitio, setNombreSitio] = useState("");
-  const [direccion, setDireccion] = useState("");
-  const [codigoPostal, setCodigoPostal] = useState("");
+  const [uso_datosmediotransportemediotransporte, setuso_datosmediotransportemediotransporte] = useState("");
+  const [uso_datosmediotransporteempresaambulancia, setuso_datosmediotransporteempresaambulancia] = useState("");
+  const [uso_datosmediotransporteobservaciones, setuso_datosmediotransporteobservaciones] = useState("");
+  const [uso_datospersonaresponsable, setuso_datospersonaresponsable] = useState("");
+  const [uso_firma, setuso_firma] = useState("");
+  
   const [errores, setErrores] = useState([]);
 
   // Función para validar todos los campos antes de navegar
   const validateFields = () => {
     const newErrors = [];
-    if (!nombreCompleto.trim())
-      newErrors.push("El nombre completo es obligatorio.");
+    if (!uso_datosmediotransportemediotransporte.trim())
+      newErrors.push("El medio de transporte es obligatorio.");
     if (
-      !documentoIdentificacion.trim() ||
-      documentoIdentificacion.length !== 8
+      !uso_datosmediotransporteempresaambulancia.trim() ||
+      uso_datosmediotransporteempresaambulancia.length < 8
     ) {
-      newErrors.push("El documento de identificación debe tener 10 dígitos.");
+      newErrors.push("La empresa debe tener al menos 8 caracteres.");
     }
-    if (!existenciaDesfibriladores.trim()) {
-      newErrors.push("Debe indicar la cantidad de desfibriladores.");
+    if (!uso_datosmediotransporteobservaciones.trim() || uso_datosmediotransporteobservaciones.length < 6 ) {
+      newErrors.push("La observación debe contener al menos 6 caracteres.");
     }
-    if (!nombreSitio.trim()) {
-      newErrors.push("El nombre del sitio es obligatorio.");
-    }
-    if (!direccion.trim()) {
-      newErrors.push("La dirección es obligatoria.");
-    }
-    if (!codigoPostal.trim() || !/^\d{5}$/.test(codigoPostal)) {
-      newErrors.push("El código postal debe tener 5 dígitos.");
+    if (uso_datospersonaresponsable.length < 8 ) {
+      newErrors.push("El nombre del responsable debe contener al menos 8 caracteres.");
     }
 
     setErrores(newErrors);
@@ -142,17 +76,89 @@ export default function Options() {
   };
 
   // Función que se llama cuando el usuario intenta navegar a la siguiente sección
-  const navigateToSection = () => {
+  const navigateToSection = async () => {
     if (validateFields()) {
       // Guardar la información en el localStorage antes de navegar
-      localStorage.setItem("REG_NOMBRECOMPLETO", nombreCompleto);
-      localStorage.setItem("REG_DOCIDENTIFICACION", documentoIdentificacion);
-      localStorage.setItem("REG_CANTIDAD", existenciaDesfibriladores);
-      localStorage.setItem("REG_NOMBREUBICACION", nombreSitio);
-      localStorage.setItem("REG_DIRECCIONUBICACION", direccion);
-      localStorage.setItem("REG_CODIGOPOSTAL", codigoPostal);
+      localStorage.setItem("uso_datosmediotransportemediotransporte", uso_datosmediotransportemediotransporte);
+      localStorage.setItem("uso_datosmediotransporteempresaambulancia", uso_datosmediotransporteempresaambulancia);
+      localStorage.setItem("uso_datosmediotransporteobservaciones", uso_datosmediotransporteobservaciones);
+      localStorage.setItem("uso_datospersonaresponsable", uso_datospersonaresponsable);
+      localStorage.setItem("uso_firma", uso_firma);
 
-      router.push("/anexo2pagina2"); // Asegúrate de que esta ruta es la correcta
+      const uso_fechaevento = localStorage.getItem("uso_fechaevento");
+      const uso_nombrelugarevento = localStorage.getItem("uso_nombrelugarevento");
+      const uso_personaatendidaeventonombre = localStorage.getItem("uso_personaatendidaeventonombre");
+      const uso_personaatendidaeventotipodocumento = localStorage.getItem("uso_personaatendidaeventotipodocumento");
+      const uso_personaatendidaeventonumerodocumento = localStorage.getItem("uso_personaatendidaeventonumerodocumento");
+      const uso_personaatendidaeventoedad = localStorage.getItem("uso_personaatendidaeventoedad");
+      const uso_personaatendidaeventosexo = localStorage.getItem("uso_personaatendidaeventosexo");
+      const uso_personaatendidaeventoaseguradorsalud = localStorage.getItem("uso_personaatendidaeventoaseguradorsalud");
+      const uso_datoseventonombrepersonautilizodea = localStorage.getItem("uso_datoseventonombrepersonautilizodea");
+      const uso_datoseventotipodocumento = localStorage.getItem("uso_datoseventotipodocumento");
+      const uso_datoseventonumerodocumento = localStorage.getItem("uso_datoseventonumerodocumento");
+      const uso_datoseventotelefono = localStorage.getItem("uso_datoseventotelefono");
+      const uso_datoseventohorainicioevento = localStorage.getItem("uso_datoseventohorainicioevento");
+      const uso_datoseventohoraactivacioncadenasupervivencia = localStorage.getItem("uso_datoseventohoraactivacioncadenasupervivencia");
+      const uso_datoseventohorautilizaciondea = localStorage.getItem("uso_datoseventohorautilizaciondea");
+      const uso_datoseventohoratrasladopersonaatendida = localStorage.getItem("uso_datoseventohoratrasladopersonaatendida");
+      const uso_datosmediotransportenombreencargado = localStorage.getItem("uso_datosmediotransportenombreencargado");
+
+      const data = {
+        uso_fechaevento:uso_fechaevento,
+        uso_nombrelugarevento:uso_nombrelugarevento,
+        uso_personaatendidaeventonombre:uso_personaatendidaeventonombre,
+        uso_personaatendidaeventotipodocumento:uso_personaatendidaeventotipodocumento,
+        uso_personaatendidaeventonumerodocumento:uso_personaatendidaeventonumerodocumento,
+        uso_personaatendidaeventoedad:uso_personaatendidaeventoedad,
+        uso_personaatendidaeventosexo:uso_personaatendidaeventosexo,
+        uso_personaatendidaeventoaseguradorsalud:uso_personaatendidaeventoaseguradorsalud,
+        uso_datoseventonombrepersonautilizodea:uso_datoseventonombrepersonautilizodea,
+        uso_datoseventotipodocumento:uso_datoseventotipodocumento,
+        uso_datoseventonumerodocumento:uso_datoseventonumerodocumento,
+        uso_datoseventotelefono:uso_datoseventotelefono,
+        uso_datoseventohorainicioevento:uso_datoseventohorainicioevento,
+        uso_datoseventohoraactivacioncadenasupervivencia:uso_datoseventohoraactivacioncadenasupervivencia,
+        uso_datoseventohorautilizaciondea:uso_datoseventohorautilizaciondea,
+        uso_datoseventohoratrasladopersonaatendida:uso_datoseventohoratrasladopersonaatendida,
+        uso_datosmediotransportenombreencargado:uso_datosmediotransportenombreencargado,
+        uso_datosmediotransportemediotransporte:uso_datosmediotransportemediotransporte,
+        uso_datosmediotransporteempresaambulancia:uso_datosmediotransporteempresaambulancia,
+        uso_datosmediotransporteobservaciones:uso_datosmediotransporteobservaciones,
+        uso_datospersonaresponsable:uso_datospersonaresponsable,
+        uso_firma:uso_firma
+      };
+
+      try {
+        const jsonBody = JSON.stringify(data);
+        const response = await fetch(
+          "https://dea-crud.azurewebsites.net/apiUso.php",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          }
+        );
+        //alert(JSON.stringify(data))
+        if (response.ok) {
+          const responseData = await response.json();
+          if (responseData.message === "dea creado") {
+            // La solicitud se completó con éxito y el mensaje es "dea creado"
+            router.push("/anexo3finalizo"); // Asegúrate de que esta ruta es la correcta
+          } else {
+            // El mensaje no es "dea creado"
+            alert("El DEA no se pudo registrar. Por favor, intente de nuevo.");
+          }
+        } else {
+          // Hubo un error en la solicitud
+          alert("Error al enviar los datos.");
+        }
+      } catch (error) {
+        console.error("Error en la solicitud:", error);
+      }
+
+      
     } else {
       // Mostrar los mensajes de error si la validación falla
       errores.forEach((error) => alert(error));
@@ -176,7 +182,7 @@ export default function Options() {
           <select
             className="inputData"
             onChange={(e) =>
-              setUso_datosmediotransportemediotransporte(e.target.value)
+              setuso_datosmediotransportemediotransporte(e.target.value)
             }
           >
             <option value="seleccione">Seleccione uno</option>
@@ -184,48 +190,60 @@ export default function Options() {
             <option value="Transporte particular">Transporte particular</option>
             <option value="Otro">Otro</option>
           </select>
+          {errores.includes("El medio de transporte es obligatorio.") && (
+            <p className="text-red-500">El medio de transporte es obligatorio.</p>
+          )}
         </div>
 
         <div className="bloque">
           <input
             type="text"
-            id="edad"
+            id="uso_datosmediotransporteempresaambulancia"
             placeholder="Nombre de la empresa de la ambulancia"
             className="inputData"
             onChange={(e) =>
-              setUso_datosmediotransporteempresaambulancia(e.target.value)
+              setuso_datosmediotransporteempresaambulancia(e.target.value)
             }
           />
+          {errores.includes("La empresa debe tener al menos 8 caracteres.") && (
+            <p className="text-red-500">La empresa debe tener al menos 8 caracteres.</p>
+          )}
         </div>
         <br></br>
         <div className="bloque">
           <strong>Observaciones:</strong>
           <textarea
-            id="edad"
+            id="uso_datosmediotransporteobservaciones"
             placeholder=""
             className="inputData"
             onChange={(e) =>
-              setUso_datosmediotransporteobservaciones(e.target.value)
+              setuso_datosmediotransporteobservaciones(e.target.value)
             }
           ></textarea>
+          {errores.includes("La observación debe contener al menos 8 caracteres.") && (
+            <p className="text-red-500">La observación debe contener al menos 8 caracteres.</p>
+          )}
         </div>
 
         <div className="bloque">
           <input
             type="text"
-            id="edad"
+            id="uso_datospersonaresponsable"
             placeholder="Nombre persona de la institución responsable del DEA utilizado"
             className="inputData"
-            onChange={(e) => setUso_firma(e.target.value)}
+            onChange={(e) => setuso_datospersonaresponsable(e.target.value)}
           />
+          {errores.includes("El nombre del responsable debe contener al menos 8 caracteres.") && (
+            <p className="text-red-500">El nombre del responsable debe contener al menos 8 caracteres.</p>
+          )}
         </div>
         <div className="bloqueFirma">
           <strong>Firma:</strong>
           <input
             type="text"
-            id="edad"
+            id="uso_firma"
             className="inputData"
-            onChange={(e) => setUso_firma(e.target.value)}
+            onChange={(e) => setuso_firma(e.target.value)}
           />
         </div>
 
